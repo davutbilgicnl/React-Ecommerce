2d2740cdc6b90c9171291fce8d86aeed609552ed
davutbilgicnl <davutbilgic.nl@gmail.com>
2023-12-30 14:46:56 +0100

    Addin the "Cart" Button Component + Header is made responsive

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import headerImage from "../../assets/meals.jpg";
 import styles from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 export default function Header() {
   return (
     <>
       <header className={styles.header}>
         <h1>PAN Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={styles["main-image"]}>
         <img src={headerImage} alt="A table full of delicious food!" />
