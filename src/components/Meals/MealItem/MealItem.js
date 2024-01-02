cf86bac10af799432dbbdcff490ff4d5e11dd043
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-01-02 15:14:24 +0100

    Working with Refs & Forward Refs

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,21 @@
+import { useContext } from "react";
 import styles from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 
 export default function MealItem({ id, name, description, price }) {
+  const cartCtx = useContext(CartContext);
   const _price = `$${price.toFixed(2)}`;
+
+  function addToCartHandler(amount) {
+    cartCtx.addItem({
+      id: id,
+      name: name,
+      amount: amount,
+      price: price,
+    });
+  }
+
   return (
     <li className={styles.meal}>
       <div>
@@ -11,7 +24,7 @@ export default function MealItem({ id, name, description, price }) {
         <div className={styles.price}>{_price}</div>
       </div>
       <div>
-        <MealItemForm id={id} />
+        <MealItemForm id={id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,10 +1,11 @@
+import { forwardRef } from "react";
 import styles from "./Input.module.css";
 
-export default function Input({ ...props }) {
+export default forwardRef(function Input({ ...props }, ref) {
   return (
     <div className={styles.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input {...props.input} ref={ref} />
     </div>
   );
-}
+});
